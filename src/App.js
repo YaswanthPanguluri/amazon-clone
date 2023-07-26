25b342b4c16c60d1526d6ca3621f95dcdd8dfe40
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-07-26 17:36:09 -0500

    Router setup Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hai yaswanth</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<h1>Home Page!!</h1>} />
+          <Route path="/login" element={<h1>Login Page</h1>} />
+          <Route path="/checkout" element={<h1>Checkout</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
